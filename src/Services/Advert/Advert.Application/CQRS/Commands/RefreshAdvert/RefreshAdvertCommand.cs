b705bde09b482665bada1d2c07b96a8f68fbe72a
AntonShcherba <scherba891@gmail.com>
2025-03-23 18:15:34 +0300

    modify refresh advert use case

--- src/Services/Advert/Advert.Application/CQRS/Commands/RefreshAdvert/RefreshAdvertCommand.cs
+++ src/Services/Advert/Advert.Application/CQRS/Commands/RefreshAdvert/RefreshAdvertCommand.cs
@@ -1,6 +1,7 @@
 using Advert.Application.Common.Advert.Models;
+using FluentResults;
 using MediatR;
 
 namespace Advert.Application.CQRS.Commands.RefreshAdvert;
 
-public record RefreshAdvertCommand(int Id) : IRequest<AdvertResponse>;
+public record RefreshAdvertCommand(int Id) : IRequest<Result<AdvertResponse>>;
