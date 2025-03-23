09820c702f395c481cc2bd7a5c9e4be9b4b21edc
AntonShcherba <scherba891@gmail.com>
2025-03-23 16:41:26 +0300

    modify Advert.API/Program.cs

--- src/Services/Advert/Advert.API/Program.cs
+++ src/Services/Advert/Advert.API/Program.cs
@@ -1,17 +1,18 @@
-using System.Reflection;
 using Advert.Application.Extensions;
 using Advert.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationServices();
-
 builder.Services.AddInfrastructureServices(builder.Configuration);
-//builder.Services.Configure<NbrbApiConfig>(builder.Configuration.GetSection(nameof(NbrbApiConfig)));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
 builder.Services.AddControllers().AddNewtonsoftJson();
-    //.AddNewtonsoftJson(options => options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore);
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -24,6 +25,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAll");
 app.MapControllers();
 
 app.Run();
+
+// 4 уровня зрелости API Ридчардсона
