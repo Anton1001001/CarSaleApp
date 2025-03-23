3fdeda74a7ca555ee72869d09cd58bf05fecb54e
AntonShcherba <scherba891@gmail.com>
2025-03-23 17:34:57 +0300

    add cancallation token to CurrencyConverrter.cs

--- src/Services/Advert/Advert.Application/Helpers/CurrencyConverter.cs
+++ src/Services/Advert/Advert.Application/Helpers/CurrencyConverter.cs
@@ -1,29 +1,34 @@
+using Advert.Application.Abstractions;
+using Advert.Application.Common.Advert.Models;
 using Advert.Application.CQRS.Commands.CreateAdvert;
-using Advert.Application.Interfaces;
 using Advert.Domain.Enums;
 
 namespace Advert.Application.Helpers;
 
 public class CurrencyConverter(ICurrencyRateService currencyRateService) : ICurrencyConverter
 {
-    public async Task<PriceResponse> ConvertPriceToAllCurrenciesAsync(int amount, Currency currency)
+    public async Task<PriceResponse?> ConvertPriceToAllCurrenciesOrDefaultAsync(int amount, Currency currency, CancellationToken cancellationToken = default)
     {
-        var rates = await currencyRateService.GetCurrencyRatesAsync();
+        var rates = await currencyRateService.GetCurrencyRatesAsync(cancellationToken);
         var amountInByn = currency switch
         {
             Currency.Usd => amount * rates.Usd,
             Currency.Eur => amount * rates.Eur,
             Currency.Rub => amount * rates.Rub,
             Currency.Byn => amount,
-            _ => throw new ArgumentOutOfRangeException(nameof(currency), currency, null)
+            _ => default(decimal?)
         };
 
-        return new PriceResponse
+        if (amountInByn is null)
         {
-            Usd = Convert.ToInt32(amountInByn / rates.Usd),
-            Eur = Convert.ToInt32(amountInByn / rates.Eur),
-            Rub = Convert.ToInt32(amountInByn / rates.Rub),
-            Byn = Convert.ToInt32(amountInByn)
-        };
+            return null;
+        }
+
+        return new PriceResponse(
+            Usd: Convert.ToInt32(amountInByn / rates.Usd),
+            Eur: Convert.ToInt32(amountInByn / rates.Eur),
+            Rub: Convert.ToInt32(amountInByn / rates.Rub),
+            Byn: Convert.ToInt32(amountInByn)
+        );
     }
 }
