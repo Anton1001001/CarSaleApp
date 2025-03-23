5c412bb024d0193588c97f3336b0be173d8f891c
AntonShcherba <scherba891@gmail.com>
2025-03-23 19:10:57 +0300

    add file service grpc client

---
+++ src/Services/Advert/Advert.Infrastructure/GrpcClients/FileService/FileServiceGrpcClient.cs
@@ -0,0 +1,18 @@
+using Advert.Application.Abstractions.GrpcClients;
+using Advert.Application.Common.Advert.Models;
+using AutoMapper;
+using File.GrpcService;
+
+namespace Advert.Infrastructure.GrpcClients.FileService;
+
+public class FileServiceGrpcClient(File.GrpcService.File.FileClient client, IMapper mapper) : IFileServiceGrpcClient 
+{
+    public async Task<List<PhotoResponse>> GetFilesByIdsResponseAsync(List<int> ids, CancellationToken cancellationToken)
+    {
+        var request = new GetFilesByIdsRequest();
+        request.Ids.AddRange(ids);
+        var photos = await client.GetFilesByIdsAsync(request, cancellationToken: cancellationToken);
+        var response = mapper.Map<List<PhotoResponse>>(photos.Files);
+        return response;
+    }
+}
