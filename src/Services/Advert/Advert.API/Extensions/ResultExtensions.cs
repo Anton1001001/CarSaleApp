f314fd772d61ffff5a1377359a6cc1ff27d5ae2e
AntonShcherba <scherba891@gmail.com>
2025-03-23 16:30:04 +0300

    add extensions for result

---
+++ src/Services/Advert/Advert.API/Extensions/ResultExtensions.cs
@@ -0,0 +1,69 @@
+﻿using Advert.Application.Errors.Base;
+using FluentResults;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Advert.API.Extensions;
+
+public static class ResultExtensions
+{
+    public static IResult TryGetResult<TValue>(this Result<TValue> result, Func<TValue, IResult> success)
+    {
+        return result switch
+        {
+            { IsFailed: true } => result.ToProblemDetail(),
+            { IsSuccess: true } => success(result.Value),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    public static IResult TryGetResult(this Result result, Func<IResult> success)
+    {
+        return result switch
+        {
+            { IsFailed: true } => result.ToProblemDetail(),
+            { IsSuccess: true } => success(),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    private static IResult ToProblemDetail(this ResultBase result)
+    {
+        if (result.IsSuccess)
+        {
+            throw new InvalidOperationException("Cannot create a problem because the result is successful");
+        }
+
+        var error = result.Errors.FirstOrDefault()!;
+        Dictionary<string, object?> extensions = new Dictionary<string, object?> { { "message", error.Message } };
+
+        if (error is BaseError baseError)
+        {
+            extensions.Add("code", baseError.Code);
+        }
+
+
+        return Results.Problem(
+            statusCode: GetStatusCode(error),
+            title: GetTitle(error),
+            extensions: extensions);
+    }
+
+    private static int GetStatusCode(IError error) => error switch
+    {
+        BadRequestError => StatusCodes.Status400BadRequest,
+        UnauthorizedError => StatusCodes.Status401Unauthorized,
+        NotFoundError => StatusCodes.Status404NotFound,
+        ConflictError => StatusCodes.Status409Conflict,
+        InternalServerError => StatusCodes.Status500InternalServerError,
+        _ => StatusCodes.Status500InternalServerError
+    };
+
+    private static string GetTitle(IError error) => error switch
+    {
+        UnauthorizedError => "Unauthorized",
+        NotFoundError => "Not Found",
+        ConflictError => "Conflict",
+        InternalServerError => "Internal Server Error",
+        _ => "Internal server error"
+    };
+}
