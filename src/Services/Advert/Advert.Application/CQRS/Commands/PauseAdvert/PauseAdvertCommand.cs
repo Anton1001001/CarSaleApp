7d1341d224a46b186102bd5ccc64c7c4f7b54f56
AntonShcherba <scherba891@gmail.com>
2025-03-23 18:07:57 +0300

    modify pause advert use case

--- src/Services/Advert/Advert.Application/CQRS/Commands/PauseAdvert/PauseAdvertCommand.cs
+++ src/Services/Advert/Advert.Application/CQRS/Commands/PauseAdvert/PauseAdvertCommand.cs
@@ -1,6 +1,7 @@
 using Advert.Application.Common.Advert.Models;
+using FluentResults;
 using MediatR;
 
 namespace Advert.Application.CQRS.Commands.PauseAdvert;
 
-public record PauseAdvertCommand(int Id) : IRequest<AdvertResponse>;
+public record PauseAdvertCommand(int Id) : IRequest<Result<AdvertResponse>>;
--- src/Services/Advert/Advert.Application/CQRS/Commands/PauseAdvert/PauseAdvertHandler.cs
+++ src/Services/Advert/Advert.Application/CQRS/Commands/PauseAdvert/PauseAdvertHandler.cs
@@ -1,28 +1,66 @@
 using Advert.Application.Common.Advert.Models;
-using Advert.Application.CQRS.Queries.GetAdvertById;
+using Advert.Application.Errors;
+using Advert.Application.Errors.Base;
+using Advert.Application.Services.Interfaces;
 using Advert.Domain.Constants;
-using Advert.Domain.Interfaces;
+using Advert.Domain.Interfaces.Repositories;
+using FluentResults;
 using MediatR;
 
 namespace Advert.Application.CQRS.Commands.PauseAdvert;
 
-public class PauseAdvertHandler(IUnitOfWork unitOfWork, ISender sender)
-    : IRequestHandler<PauseAdvertCommand, AdvertResponse>
+public class PauseAdvertHandler(
+    IUnitOfWork unitOfWork,
+    IAdvertService advertService)
+    : IRequestHandler<PauseAdvertCommand, Result<AdvertResponse>>
 {
-    public async Task<AdvertResponse> Handle(PauseAdvertCommand request, CancellationToken cancellationToken)
+    public async Task<Result<AdvertResponse>> Handle(PauseAdvertCommand request, CancellationToken cancellationToken)
     {
         var advert = await unitOfWork.AdvertRepository.GetByIdAsync(request.Id, cancellationToken);
 
-        advert.AdvertStatus = AdvertStatus.Paused;
-        advert.AdvertPublicStatus =
-            await unitOfWork.AdvertPublicStatusRepository.GetByNameAsync(AdvertPublicStatus.TemporaryUnavailable,
-                cancellationToken);
-        advert.AdvertPrivateStatus =
-            await unitOfWork.AdvertPrivateStatusRepository.GetByNameAsync(AdvertPrivateStatus.Paused,
-                cancellationToken);
+        if (advert is null)
+        {
+            return new AdvertNotFoundError(message: $"Advert with id: {request.Id} was not found");
+        }
 
-        await unitOfWork.SaveChangesAsync(cancellationToken);
-        var advertResponse = await sender.Send(new GetAdvertByIdQuery(advert.Id), cancellationToken);
+        advert.AdvertStatus = AdvertStatus.Paused;
+
+        var advertPublicStatus = await unitOfWork.AdvertPublicStatusRepository.GetByNameAsync(
+            AdvertPublicStatus.TemporaryUnavailable,
+            cancellationToken);
+        
+        if (advertPublicStatus is null)
+        {
+            return new InternalServerError(code: "Advert.Pause", message: "Public status error");
+        }
+
+        advert.AdvertPublicStatus = advertPublicStatus;
+        
+        var advertPrivateStatus = await unitOfWork.AdvertPrivateStatusRepository.GetByNameAsync(
+            AdvertPrivateStatus.Paused,
+            cancellationToken);
+        
+        if (advertPrivateStatus is null)
+        {
+            return new InternalServerError(code: "Advert.Pause", message: "Private status error");
+        }
+
+        advert.AdvertPrivateStatus = advertPrivateStatus;
+
+        var saveResult = await unitOfWork.SaveChangesAsync(cancellationToken);
+        
+        if (!saveResult)
+        {
+            return new InternalServerError(code: "CarAdvert.Pause", message: "Failed to save data");
+        }
+
+        var advertResponse = await advertService.GetAdvertByIdAsync(advert.Id, cancellationToken);
+
+        if (advertResponse is null)
+        {
+            return new AdvertNotFoundError(message: $"Advert with id: {advert.Id} was not found");
+        }
+
         return advertResponse;
     }
 }
