3c18a0c8b17fe796476b5955a2a36372cc47e8c8
AntonShcherba <scherba891@gmail.com>
2025-03-23 17:30:58 +0300

    add extensions for application-layer configuration

--- src/Services/Advert/Advert.Application/Extensions/Extensions.cs
+++ src/Services/Advert/Advert.Application/Extensions/Extensions.cs
@@ -1,13 +1,8 @@
 using System.Reflection;
-using Advert.Application.Common;
-using Advert.Application.Common.Advert.Models;
-using Advert.Application.CQRS.Commands.CreateAdvert;
-using Advert.Application.CQRS.Commands.CreateAdvert.Processors.CreateBusAdvert;
-using Advert.Application.CQRS.Commands.CreateAdvert.Processors.CreateCarAdvert;
-using Advert.Application.CQRS.Commands.CreateAdvert.Processors.CreateMotoAdvert;
-using Advert.Application.CQRS.Queries.GetAdvertById.Processors.GetCarAdvertById;
+using Advert.Application.Abstractions;
 using Advert.Application.Helpers;
-using Advert.Application.Interfaces;
+using Advert.Application.Services;
+using Advert.Application.Services.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Advert.Application.Extensions;
@@ -16,36 +11,15 @@ public static class Extensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        services.AddScoped<CreateCarAdvertProcessor>();
-        services.AddScoped<CreateBusAdvertProcessor>();
-        services.AddScoped<CreateMotoAdvertProcessor>();
-
-        services.AddScoped<Processor<CreateAdvertCommand, AdvertResponse>>(sp =>
-        {
-            var carHandler = sp.GetRequiredService<CreateCarAdvertProcessor>();
-            var busHandler = sp.GetRequiredService<CreateBusAdvertProcessor>();
-            var motoHandler = sp.GetRequiredService<CreateMotoAdvertProcessor>();
-
-            carHandler
-                .SetNext(busHandler)
-                .SetNext(motoHandler);
-            return carHandler;
-        });
-
-        services.AddScoped<GetCarAdvertByIdProcessor>();
-        
-        services.AddScoped<Processor<Domain.Entities.Advert, AdvertResponse>>(sp =>
-        {
-            var carHandler = sp.GetRequiredService<GetCarAdvertByIdProcessor>();
-
-            return carHandler;
-        });
-
-        services.AddSingleton<ICurrencyConverter, CurrencyConverter>();
-
         var applicationAssembly = Assembly.GetExecutingAssembly();
         services.AddAutoMapper(applicationAssembly);
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(applicationAssembly));
+        
+        services.AddScoped<IAdvertService, AdvertService>();
+        services.AddSingleton<ICurrencyConverter, CurrencyConverter>();
+
+        services.AddChainOfResponsibilityServices();
+
         return services;
     }
 }
