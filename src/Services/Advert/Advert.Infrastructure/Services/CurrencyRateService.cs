22620bfcc521640cac596bc93d9375e7266d223e
AntonShcherba <scherba891@gmail.com>
2025-03-23 19:25:32 +0300

    add redis to currency rate service

--- src/Services/Advert/Advert.Infrastructure/Services/CurrencyRateService.cs
+++ src/Services/Advert/Advert.Infrastructure/Services/CurrencyRateService.cs
@@ -1,17 +1,27 @@
+using Advert.Application.Abstractions;
 using Advert.Application.Helpers;
-using Advert.Application.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 namespace Advert.Infrastructure.Services;
 
-public class CurrencyRateService(IOptions<NbrbApiConfig> options) : ICurrencyRateService
+public class CurrencyRateService(IOptions<NbrbApiConfig> options, IDistributedCache cache) : ICurrencyRateService
 {
     private static readonly HttpClient HttpClient = new();
+    private static readonly TimeSpan CacheDuration = TimeSpan.FromDays(1); 
 
-    public async Task<CurrencyRates> GetCurrencyRatesAsync()
+    public async Task<CurrencyRates> GetCurrencyRatesAsync(CancellationToken cancellationToken = default)
     {
         var nbrbOptions = options.Value;
-        var response = await HttpClient.GetStringAsync(nbrbOptions.NbrbApiUrl);
+        var cacheKey = "CurrencyRates";
+        var cachedData = await cache.GetStringAsync(cacheKey, token: cancellationToken);
+        
+        if (!string.IsNullOrEmpty(cachedData))
+        {
+            return JsonConvert.DeserializeObject<CurrencyRates>(cachedData)!;
+        }
+        
+        var response = await HttpClient.GetStringAsync(nbrbOptions.NbrbApiUrl, cancellationToken);
         var rateList = JsonConvert.DeserializeObject<List<NbrbRate>>(response);
 
         var rates = new CurrencyRates
@@ -24,6 +34,11 @@ public class CurrencyRateService(IOptions<NbrbApiConfig> options) : ICurrencyRat
             Rub = rateList.Find(r => r.CurAbbreviation == nbrbOptions.Rub)?.CurOfficialRate /
                 rateList.Find(r => r.CurAbbreviation == nbrbOptions.Rub)?.CurScale ?? 0
         };
+        
+        await cache.SetStringAsync(cacheKey, JsonConvert.SerializeObject(rates), new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = CacheDuration
+        }, token: cancellationToken);
 
         return rates;
     }
@@ -31,7 +46,7 @@ public class CurrencyRateService(IOptions<NbrbApiConfig> options) : ICurrencyRat
     public class NbrbRate
     {
         [JsonProperty("Cur_Abbreviation")] 
-        public string CurAbbreviation { get; set; }
+        public string CurAbbreviation { get; set; } = null!;
 
         [JsonProperty("Cur_OfficialRate")] 
         public decimal CurOfficialRate { get; set; }
