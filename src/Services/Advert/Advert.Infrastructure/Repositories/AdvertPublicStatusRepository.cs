26a35d4b51febd98483ed4c17f402bfaac406c94
AntonShcherba <scherba891@gmail.com>
2025-03-23 19:17:16 +0300

    modify AdvertPublicStatusRepository

--- src/Services/Advert/Advert.Infrastructure/Repositories/AdvertPublicStatusRepository.cs
+++ src/Services/Advert/Advert.Infrastructure/Repositories/AdvertPublicStatusRepository.cs
@@ -1,12 +1,13 @@
 using Advert.Domain.Entities;
 using Advert.Domain.Interfaces;
+using Advert.Domain.Interfaces.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace Advert.Infrastructure.Repositories;
 
 public class AdvertPublicStatusRepository(AdvertDbContext context) : IAdvertPublicStatusRepository
 {
-    public async Task<AdvertPublicStatus> GetByNameAsync(string name, CancellationToken cancellationToken = default)
+    public async Task<AdvertPublicStatus?> GetByNameAsync(string name, CancellationToken cancellationToken = default)
     {
         return await context.AdvertPublicStatuses.FirstOrDefaultAsync(publicStatus => publicStatus.Name == name, cancellationToken); 
     }
