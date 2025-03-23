6d9acf8d54e0b83b1ceeb9055190c2d257684f8b
AntonShcherba <scherba891@gmail.com>
2025-03-23 16:43:14 +0300

    add logic for results in AdvertsController

--- src/Services/Advert/Advert.API/Controllers/AdvertsController.cs
+++ src/Services/Advert/Advert.API/Controllers/AdvertsController.cs
@@ -1,13 +1,16 @@
+using Advert.API.Extensions;
+using Advert.Application.Common.Advert.Models.Parameters;
 using Advert.Application.CQRS.Commands.CreateAdvert;
-using Advert.Application.CQRS.Commands.CreateAdvert.Parameters;
 using Advert.Application.CQRS.Commands.PauseAdvert;
 using Advert.Application.CQRS.Commands.PublishAdvert;
 using Advert.Application.CQRS.Commands.RefreshAdvert;
 using Advert.Application.CQRS.Commands.RemoveAdvert;
 using Advert.Application.CQRS.Queries.GetAdvertById;
+using Advert.Application.CQRS.Queries.GetAdvertCategories;
+using Advert.Application.CQRS.Queries.GetAdvertForm;
 using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json.Linq;
 
 namespace Advert.API.Controllers;
 
@@ -15,74 +18,62 @@ namespace Advert.API.Controllers;
 [Route("api/adverts/")]
 public class AdvertsController(ISender sender) : ControllerBase
 {
+    [HttpGet("categories")]
+    public async Task<IResult> GetAdvertCategories(CancellationToken cancellationToken)
+    {
+        var result = await sender.Send(new GetAdvertCategoriesQuery(), cancellationToken);
+        return Results.Ok(result);
+    }
     [HttpGet("{id:int}")]
-    public async Task<IActionResult> GetAdvertById([FromRoute] int id, CancellationToken cancellationToken = default)
+    public async Task<IResult> GetAdvertById([FromRoute] int id, CancellationToken cancellationToken = default)
     {
         var result = await sender.Send(new GetAdvertByIdQuery(id), cancellationToken);
-        return Ok(result);
+        return result.TryGetResult(Results.Ok);
     }
 
     [HttpPost("{type}/create")]
-    public async Task<IActionResult> CreateAdvert([FromRoute] string type, [FromBody] CreateAdvertRequest request, CancellationToken cancellationToken = default)
+    public async Task<IResult> CreateAdvert([FromRoute] string type, [FromBody] CreateAdvertRequest request, CancellationToken cancellationToken = default)
     {
-        var command = ParametersFactory.CreateCommand(type, request.Params.ToString());
-        var result = await sender.Send(command, cancellationToken);
-        return Ok(result);
+        var parameters = ParametersFactory.CreateCommand(type, request.Params.ToString());
+        var result = await sender.Send(new CreateAdvertCommand(parameters), cancellationToken);
+        return result.TryGetResult(Results.Ok);
     }
     
     [HttpPost("{id}/publish")]
-    public async Task<IActionResult> PublishAdvert(int id, CancellationToken cancellationToken = default)
+    public async Task<IResult> PublishAdvert(int id, CancellationToken cancellationToken = default)
     {
         var result = await sender.Send(new PublishAdvertCommand(id), cancellationToken);
-        return Ok(result);
+        return result.TryGetResult(Results.Ok);
     }
     
     [HttpPost("{id}/pause")]
-    public async Task<IActionResult> PauseAdvert(int id, CancellationToken cancellationToken = default)
+    public async Task<IResult> PauseAdvert(int id, CancellationToken cancellationToken = default)
     {
         var result = await sender.Send(new PauseAdvertCommand(id), cancellationToken);
-        return Ok(result);
+        return result.TryGetResult(Results.Ok);
     }
 
     [HttpPost("{id}/refresh")]
-    public async Task<IActionResult> RefreshAdvert(int id, CancellationToken cancellationToken = default)
+    public async Task<IResult> RefreshAdvert(int id, CancellationToken cancellationToken = default)
     {
         var result = await sender.Send(new RefreshAdvertCommand(id), cancellationToken);
-        return Ok(result);
+        return result.TryGetResult(Results.Ok);
     }
     
     [HttpPost("{id}/remove")]
-    public async Task<IActionResult> RemoveAdvert(int id, [FromBody] RemoveAdvertCommand request, CancellationToken cancellationToken = default)
+    public async Task<IResult> RemoveAdvert(int id, [FromBody] RemoveAdvertCommand request, CancellationToken cancellationToken = default)
     {
         request = request with { Id = id };
         var result = await sender.Send(request, cancellationToken);
-        return Ok(result);
+        return result.TryGetResult(Results.Ok);
     }
     
-    [HttpGet("offer-types/{type}/offer-form")]
-    public Task<IAsyncResult> GetAdvertForm()
+    [HttpPost("{type}/form")]
+    public async Task<IResult> Form([FromRoute] string type, GetAdvertFormRequest request, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var parameters = ParametersFactory.CreateCommand(type, request.Params.ToString());
+        var result = await sender.Send(new GetAdvertFormQuery(parameters), cancellationToken);
+        return result.TryGetResult(Results.Ok);
     }
-    
-    [HttpPost("offer-types/{type}/offer-form/update")]
-    public Task<IActionResult> UpdateForm()
-    {
-        throw new NotImplementedException();
-    }
-
-    [HttpPut("offer-types/{type}/offer-form/draft")]
-    public Task<IActionResult> OfferDraft()
-    {
-        throw new NotImplementedException();
-    }
-
-    [HttpPut("offers/{id}")]
-    public Task<IActionResult> UpdateAdvert()
-    {
-        throw new NotImplementedException();
-    }
-
-    
-
 }
+
