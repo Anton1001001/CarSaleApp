e543d1e59923d95188f3c1391c1151fcc56c16bb
AntonShcherba <scherba891@gmail.com>
2025-03-23 17:11:10 +0300

    add abstractions for currency convertion

---
+++ src/Services/Advert/Advert.Application/Abstractions/ICurrencyConverter.cs
@@ -0,0 +1,10 @@
+using Advert.Application.Common.Advert.Models;
+using Advert.Application.CQRS.Commands.CreateAdvert;
+using Advert.Domain.Enums;
+
+namespace Advert.Application.Abstractions;
+
+public interface ICurrencyConverter
+{
+    Task<PriceResponse?> ConvertPriceToAllCurrenciesOrDefaultAsync(int amount, Currency currency);
+}
---
+++ src/Services/Advert/Advert.Application/Abstractions/ICurrencyRateService.cs
@@ -0,0 +1,8 @@
+using Advert.Application.Helpers;
+
+namespace Advert.Application.Abstractions;
+
+public interface ICurrencyRateService
+{
+    Task<CurrencyRates> GetCurrencyRatesAsync();
+}
