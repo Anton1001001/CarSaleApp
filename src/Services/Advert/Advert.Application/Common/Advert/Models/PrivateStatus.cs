c7a664dfcffc5825e1746504e2af0a761e3b5581
AntonShcherba <scherba891@gmail.com>
2025-03-24 04:33:00 +0300

    rename status classes

--- src/Services/Advert/Advert.Application/Common/Advert/Models/PrivateStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace Advert.Application.Common.Advert.Models;
-
-public record PrivateStatus(
-    string Name,
-    string? Label,
-    bool Published,
-    string? PhotoLabel
-);
-
--- src/Services/Advert/Advert.Application/Common/Advert/Models/PublicStatus.cs
+++
@@ -1,6 +0,0 @@
-namespace Advert.Application.Common.Advert.Models;
-
-public record PublicStatus(
-    string Label, 
-    string Name
-);
