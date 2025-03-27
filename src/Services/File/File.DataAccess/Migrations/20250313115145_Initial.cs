69055c608f30512f2a6e163023169c34833f472e
AntonShcherba <scherba891@gmail.com>
2025-03-28 00:54:28 +0300

    add migrations

---
+++ src/Services/File/File.DataAccess/Migrations/20250313115145_Initial.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace File.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Big_Width = table.Column<int>(type: "int", nullable: false),
+                    Big_Height = table.Column<int>(type: "int", nullable: false),
+                    Big_Url = table.Column<string>(type: "longtext", nullable: false, collation: "utf8mb4_general_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Medium_Width = table.Column<int>(type: "int", nullable: false),
+                    Medium_Height = table.Column<int>(type: "int", nullable: false),
+                    Medium_Url = table.Column<string>(type: "longtext", nullable: false, collation: "utf8mb4_general_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Small_Width = table.Column<int>(type: "int", nullable: false),
+                    Small_Height = table.Column<int>(type: "int", nullable: false),
+                    Small_Url = table.Column<string>(type: "longtext", nullable: false, collation: "utf8mb4_general_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ExtraSmall_Width = table.Column<int>(type: "int", nullable: false),
+                    ExtraSmall_Height = table.Column<int>(type: "int", nullable: false),
+                    ExtraSmall_Url = table.Column<string>(type: "longtext", nullable: false, collation: "utf8mb4_general_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_general_ci");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Photos");
+        }
+    }
+}
---
+++ src/Services/File/File.DataAccess/Migrations/20250313152833_add-mimeType_column_to_photo.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace File.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addmimeType_column_to_photo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MimeType",
+                table: "Photos",
+                type: "longtext",
+                nullable: false,
+                collation: "utf8mb4_general_ci")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MimeType",
+                table: "Photos");
+        }
+    }
+}
