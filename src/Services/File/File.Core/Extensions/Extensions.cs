57a572d9e8fdb3f38c7c9daa00e22bb74a7d4d41
AntonShcherba <scherba891@gmail.com>
2025-03-28 00:32:02 +0300

    add File.Core dependency injection extensions

---
+++ src/Services/File/File.Core/Extensions/Extensions.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+using File.Core.Messaging;
+using File.Core.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace File.Core.Extensions;
+
+public static class Extensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.Configure<MessageQueueOptions>(configuration.GetSection(nameof(MessageQueueOptions)));
+        services.Configure<RabbitMqOptions>(configuration.GetSection(nameof(RabbitMqOptions)));
+        
+        var applicationAssembly = Assembly.GetExecutingAssembly();
+        services.AddAutoMapper(applicationAssembly);
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(applicationAssembly));
+        services.AddSingleton<IRabbitMqConnection, RabbitMqConnection>();
+
+        return services;
+    }
+}
