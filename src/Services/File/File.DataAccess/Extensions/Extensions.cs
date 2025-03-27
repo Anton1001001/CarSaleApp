2efdb8ed37e7734eec326ffb1d0911307b667b7e
AntonShcherba <scherba891@gmail.com>
2025-03-28 00:55:48 +0300

    add File.DataAccess dependency injection extensions

---
+++ src/Services/File/File.DataAccess/Extensions/Extensions.cs
@@ -0,0 +1,44 @@
+using Amazon.Runtime;
+using Amazon.S3;
+using File.Core.Abstractions;
+using File.DataAccess.Options;
+using File.DataAccess.Repositories;
+using File.DataAccess.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace File.DataAccess.Extensions;
+
+public static class Extensions
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    { 
+        var connectionString = configuration.GetConnectionString("FileDb")!;
+        var serverVersion = ServerVersion.AutoDetect(connectionString);
+        services.AddDbContext<FileDbContext>(options => options.UseMySql(connectionString, serverVersion));
+        
+        
+        services.Configure<BackblazeB2Options>(configuration.GetSection(nameof(BackblazeB2Options)));
+
+
+        services.AddSingleton<IAmazonS3>(provider =>
+        {
+            var options = provider.GetRequiredService<IOptions<BackblazeB2Options>>().Value;
+            var config = new AmazonS3Config
+            {
+                ServiceURL = options.ServiceUrl,
+                ForcePathStyle = true
+            };
+            var credentials = new BasicAWSCredentials(options.KeyId, options.ApplicationKey);
+            return new AmazonS3Client(credentials, config);
+        });
+        services.AddScoped<IStorageService, BackblazeB2Service>();
+        services.AddScoped<IPhotoRepository, PhotoRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        
+        return services;
+    }
+}
