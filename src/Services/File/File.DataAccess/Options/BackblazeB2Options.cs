360036ccec454bafd70104c27f6ea1114f6c0554
AntonShcherba <scherba891@gmail.com>
2025-03-28 00:52:22 +0300

    add options models

---
+++ src/Services/File/File.DataAccess/Options/BackblazeB2Options.cs
@@ -0,0 +1,11 @@
+namespace File.DataAccess.Options;
+
+public class BackblazeB2Options
+{
+    public string KeyId { get; set; } = string.Empty;
+    public string KeyName { get; set; } = string.Empty;
+    public string ApplicationKey { get; set; } = string.Empty;
+    public string BucketName { get; set; } = string.Empty;
+    public string ServiceUrl { get; set; } = string.Empty;
+    public string Endpoint { get; set; } = string.Empty;
+}
