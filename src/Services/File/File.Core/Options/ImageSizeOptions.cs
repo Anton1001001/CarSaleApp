abb70f19f5bf4e8deab278c92621c290cd9245a4
AntonShcherba <scherba891@gmail.com>
2025-03-30 05:37:33 +0300

    add ImageSizeOptions POCO class

---
+++ src/Services/File/File.Core/Options/ImageSizeOptions.cs
@@ -0,0 +1,9 @@
+namespace File.Core.Options;
+
+public class ImageSizeOptions
+{
+    public int MaxLength { get; set; }
+    public int MediumLength { get; set; }
+    public int SmallLength  { get; set; }
+    public int ExtraSmallLength { get; set; }
+}
