71d92d38fca4ff5b022b7e7b6b1f2ec6b0177346
AntonShcherba <scherba891@gmail.com>
2025-03-31 16:34:39 +0300

    add application builder extensions

---
+++ src/Services/Advert/Advert.Application/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+using Advert.Application.JobManging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Advert.Application.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static void UseHangfireJobs(this IApplicationBuilder app)
+    {
+        var photosIdsSendingJob = app.ApplicationServices.GetRequiredService<IPhotosIdsSendingJob>();
+        photosIdsSendingJob.ScheduleJob();
+    }
+}
