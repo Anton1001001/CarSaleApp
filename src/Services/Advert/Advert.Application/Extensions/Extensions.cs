7511aa23984c1cacba56c5b727672bfa2a8590e3
AntonShcherba <scherba891@gmail.com>
2025-03-31 16:36:33 +0300

    add additional configuration to application dependency injection extension

--- src/Services/Advert/Advert.Application/Extensions/Extensions.cs
+++ src/Services/Advert/Advert.Application/Extensions/Extensions.cs
@@ -1,21 +1,25 @@
 using System.Reflection;
 using Advert.Application.Abstractions;
 using Advert.Application.Helpers;
+using Advert.Application.Options;
 using Advert.Application.Services;
 using Advert.Application.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Advert.Application.Extensions;
 
 public static class Extensions
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.Configure<MessageQueueOptions>(configuration.GetSection(nameof(MessageQueueOptions)));
         services.AddChainOfResponsibilityServices();
         var applicationAssembly = Assembly.GetExecutingAssembly();
         services.AddAutoMapper(applicationAssembly);
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(applicationAssembly));
-        
+
+        services.AddScoped<IPhotosSenderService, PhotosSenderService>();
         services.AddScoped<IAdvertService, AdvertService>();
         services.AddSingleton<ICurrencyConverter, CurrencyConverter>();
         
