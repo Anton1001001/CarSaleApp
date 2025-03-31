2492f65e649ee1363328096c5f98b000d4f9da94
AntonShcherba <scherba891@gmail.com>
2025-03-31 16:25:23 +0300

    create mappings for cars

--- src/Services/Advert/Advert.Application/Common/Cars/CarsMappingProfile.cs
+++ src/Services/Advert/Advert.Application/Common/Cars/CarsMappingProfile.cs
@@ -1,4 +1,5 @@
 using Advert.Application.Common.Cars.Models;
+using Advert.Application.Extensions;
 using AutoMapper;
 
 namespace Advert.Application.Common.Cars;
@@ -7,7 +8,13 @@ public class CarsMappingProfile : Profile
 {
     public CarsMappingProfile()
     {
-        CreateMap<CarsCatalogResponse, CarParametersResponse>();
+        CreateMap<CarsCatalogResponse, CarParametersResponse>()
+            .ForCtorParam(dest => dest.Year, opt =>
+                opt.MapFrom(src => -1))
+            .ForCtorParam(dest => dest.MileageKm, opt =>
+                opt.MapFrom(src => -1))
+            .ForCtorParam(dest => dest.Registration, opt =>
+                opt.MapFrom(src => default(string?)));
         CreateMap<CarParametersToSave, CarParametersResponse>();
         CreateMap<CarParametersToSave, CarsCatalogRequest>();
     }
