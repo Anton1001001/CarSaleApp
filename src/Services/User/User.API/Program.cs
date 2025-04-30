70010b4ebd43d5c7e31b5ad3c5ea17067e28f5c7
AntonShcherba <scherba891@gmail.com>
2025-04-30 17:23:49 +0300

    add User.API

--- src/Services/User/User.API/Program.cs
+++ src/Services/User/User.API/Program.cs
@@ -1,21 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Text;
+using Auth.Shared;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using User.API.Middlewares;
+using User.Core.Extensions;
+using User.Core.Options;
+using User.DataAccess.Extensions;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile(Path.Combine(AppContext.BaseDirectory, "appsettings.jwt.json"), optional: false, reloadOnChange: true);
+
+builder.Services.AddCoreServices(builder.Configuration);
+builder.Services.AddDataAccessServices(builder.Configuration);
+
+builder.Services.AddJwtAuthentication(builder.Configuration);
+builder.Services.AddAuthorization();
+
+builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
